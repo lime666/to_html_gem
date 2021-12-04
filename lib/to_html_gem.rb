641af8b0401bc553bee485392b52853d2fc8d656
Yulia Lime <lime666lime@gmail.com>
2021-12-04 18:47:58 +0200

    needs fixes

--- lib/to_html_gem.rb
+++ lib/to_html_gem.rb
@@ -1,12 +1,9 @@
-#f.readlines[0]   #=> "This is line one"
 class Html
 
-def initialize content
+def to_html(content, bypass_html: true)
   @content = content
-end
-  
-def to_html
-  convert_content
+  @paste << "<p>@content.gsub(/^[а-яА-Я ]*$/)</p>"
+    
   @html = <<HTML
     <! doctype html>
 
@@ -23,17 +20,12 @@ def to_html
     </html>
 HTML
 
-end
-
-private
-
-def convert_content
-  @html_file = File.open("index.html", w) {|f| f.write @html }
-  @paste << "<p>@content.gsub(/^[а-яА-Я ]*$/)</p>"
+  @html_file = File.open('index.html', w) {|f| f.write @html }
   @html_file.close
+
 end
 
-def open_html
+def open_html(html_file = 'index.html')
   to_html    
   system("xdg-open #{@html_file}")
 end
---
+++ to_html_gem.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name        = 'to_html_gem'
+  s.version     = '0.0.4'
+  s.summary     = "text to html"
+  s.description = "Transforming text files to html"
+  s.authors     = ["Yulia Lime"]
+  s.email       = 'lime666lime@gmail.com'
+  s.files       = ["lib/to_html_gem.rb"]
+  s.license       = 'MIT'
+end
