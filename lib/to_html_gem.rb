9ee628cd6cb08d2089249f31735a93f46ec32947
Yulia Lime <lime666lime@gmail.com>
2021-12-07 14:51:36 +0200

    added edit method

--- lib/to_html_gem.rb
+++ lib/to_html_gem.rb
@@ -1,24 +1,34 @@
 class Html
-
-def to_html(content, html_file = 'index.html')
-  paste = content.gsub(/^[а-яА-Я ]*$/, '')
+  def to_html(content, html_file = 'index.html')
+    bypass_html: true
+    @paste = @content.gsub(/^[а-яА-Я ]*$/, '')
     
-  html_file = File.new('index.html', "w+")
-  html_file.puts "<!DOCTYPE html>"
-  html_file.puts "  <head>"
-  html_file.puts "    <meta charset='utf-8'>"
-  html_file.puts "    <title>My Tamago</title>"
-  html_file.puts "  </head>"
-  html_file.puts "  <body>"
-  html_file.puts "    #{paste}"
-  html_file.puts "  </body>"
-  html_file.puts "</html>"
-  html_file.close
+    html_file = File.new('index.html', "w+")
+    html_file.puts "<!DOCTYPE html>"
+    html_file.puts "<html lang="en">"
+    html_file.puts "  <head>"
+    html_file.puts "    <meta charset='utf-8'>"
+    html_file.puts "    <title>My Tamago</title>"
+    html_file.puts "  </head>"
+    html_file.puts "  <body>"
+    html_file.puts "    #{@paste}"
+    html_file.puts "  </body>"
+    html_file.puts "</html>"
+    html_file.close
 
-end
+  end
 
-def open_html(html_file = 'index.html')
-  system("xdg-open #{html_file}")
-end
+  def edit_html(content, html_file = 'index.html')
+    @paste << @content
+    html_file = File.open(html_file, "w+")
+    html_file.puts @paste
+    html_file.close
+    file = File.open('index.html', w) {|f| f.write html_file }
+    @paste << "<p>@content.gsub(/^[а-яА-Я ]*$/)</p>"
+    @file.close
+  end
 
+  def open_html(html_file = 'index.html')
+    system("xdg-open #{html_file}")
+  end
 end
--- to_html_gem.gemspec
+++ to_html_gem.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'to_html_gem'
-  s.version     = '0.0.13'
+  s.version     = '0.0.14'
   s.summary     = "text to html"
   s.description = "Transforming text files to html"
   s.authors     = ["Yulia Lime"]
