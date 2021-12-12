9ecdcdd2db9ba949dfbf50ea990dbe570ad499f4
Yulia Lime <lime666lime@gmail.com>
2021-12-12 23:00:02 +0200

    gemspec changes

--- lib/to_html_gem.rb
+++ lib/to_html_gem.rb
@@ -1,22 +1,22 @@
 require 'nokogiri'
 
 class Html
-  def to_html(content, bypass_html: true, html_file = 'index.html')
+  def to_html(content, bypass_html = true, html_file = 'index.html')
     @paste = content.gsub!(/^[а-яА-Я ]*$/, '') if bypass_html
     
     html_file = File.new('index.html', 'w+')
-    html_file.puts '<!DOCTYPE html>'
-    html_file.puts '  <head>'
-    html_file.puts '    <meta charset='utf-8'>'
-    html_file.puts '    <title>odoodo</title>'
-    html_file.puts '    <script>'
-    html_file.puts '      setInterval(()=>{ window.location.reload() }, 2000)'
-    html_file.puts '    </script>'
-    html_file.puts '  </head>'
-    html_file.puts '  <body>'
-    html_file.puts '    #{@paste}'
-    html_file.puts '  </body>'
-    html_file.puts '</html>'
+    html_file.puts "<!DOCTYPE html>"
+    html_file.puts "  <head>"
+    html_file.puts "    <meta charset='utf-8'>"
+    html_file.puts "    <title>odoodo</title>"
+    html_file.puts "    <script>"
+    html_file.puts "      setInterval(()=>{ window.location.reload() }, 2000)"
+    html_file.puts "    </script>"
+    html_file.puts "  </head>"
+    html_file.puts "  <body>"
+    html_file.puts "    #{@paste}"
+    html_file.puts "  </body>"
+    html_file.puts "</html>"
     html_file.close
 
   end
--- to_html_gem.gemspec
+++ to_html_gem.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name        = 'to_html_gem'
-  s.version     = '0.0.20'
-  s.summary     = 'text to html'
-  s.description = 'Transforming text files to html'
-  s.authors     = ['Yulia Lime']
+  s.version     = '0.0.23'
+  s.summary     = "text to html"
+  s.description = "Transforming text files to html"
+  s.authors     = ["Yulia Lime"]
   s.email       = 'lime666lime@gmail.com'
-  s.files       = ['lib/to_html_gem.rb']
+  s.files       = ["lib/to_html_gem.rb"]
   s.license     = 'MIT'
 end
